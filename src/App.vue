830d9dbd0f417044262e3f936fb0131b5416640d
SunKnight002 <leosun6582@gmail.com>
2024-08-22 20:38:31 +0800

    大概處理的vue router，但是我的index仍然沒有內容出現

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,59 @@
 <script setup>
+import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import FontPage from './components/FontPage.vue';
+
+// 定義 showMenuBar 函數
+const menuBarVisible = ref(false);
+
+const showMenuBar = () => {
+  menuBarVisible.value = !menuBarVisible.value
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+  <header class="top">
+    <RouterLink to="/"><img src="@/assets/img/memelogo.svg" alt="Meme Logo" /></RouterLink>
+    <i class="fa fa-bars" aria-hidden="true" @click="showMenuBar"></i>
+        <ul id="menuBar">
+          <li><RouterLink to="/fontpage">線上體驗</RouterLink></li>
+          <li><RouterLink to="/about">遊戲主題</RouterLink></li>
+          <li><RouterLink to="/member">會員專區</RouterLink></li>
+        </ul>
   </header>
+
+  <section class="main">
+        <section class="wrapper">
+          <h1>超級無敵太陽騎士</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ea nostrum natus, ducimus deserunt sunt maxime? Corporis ad, repellat perferendis molestias quo totam inventore sint officia excepturi, dolorem, ratione ut?</p>
+            <p class="spec">Lorem ipsum dolor sit amet consectetur.</p>
+            <h1>超級無敵太陽騎士</h1>
+        </section>
+    </section>
+
+    <footer>
+        <div class="bottom">
+            <div><img src="./src/assets/img/memelogo.svg" alt=""></div>
+
+            <div class="bm_text">
+                <p>© Copyright Meme謎因工作室&nbsp;&nbsp;All Rights Reserved.</p>
+                <p>本網站為緯育TibaMe【第93期】前端工程師專業技術養成班學員作品，僅供學習、展示之用途。</p>
+            </div>
+
+            <div class="icon">
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-square-threads"></i>
+            </div>
+        </div>
+    </footer>
 
   <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import FontPage from '../components/FontPage.vue' // 引入 FontPage.vue
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/fontpage', // 添加 FontPage 的路由
+      name: 'fontpage',
+      component: FontPage
     }
   ]
 })
